e54af0cc86db63835ec1c25c22562df7175e2056
GetmanYri <noglum@yandex.ru>
2023-06-06 16:52:28 +0300

    Task 63 done.

--- src/util/Util.java
+++ src/util/Util.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Util {
@@ -8,13 +9,25 @@ public class Util {
         Scanner util = new Scanner(System.in);
         return util.nextInt();
     }
+
     public static double scannerDouble() {
         Scanner util = new Scanner(System.in);
         return util.nextDouble();
     }
+
     public static String scannerString() {
         Scanner util = new Scanner(System.in);
         return util.nextLine();
     }
 
+    public static String randomString() {
+        Random r = new Random();
+        return r.ints(97, 123)
+                .limit(7)
+                .mapToObj(c -> (char) c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
+                .toString();
+
+    }
 }
+
+
